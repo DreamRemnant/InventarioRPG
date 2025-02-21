58d124df1067e3f0436596005b7e2dc7a01e896b
DreamRemnant <jfmg.64@gmail.com>
2025-02-21 13:38:40 -0700

    Se creó archivo de base de datos

--- equipo_db.sql
+++ equipo_db.sql
@@ -4,25 +4,25 @@ SET time_zone = "+00:00";
 
 CREATE DATABASE equipo_db;
 
-USE DATABASE equipo_db;
+USE equipo_db;
 
 CREATE TABLE Equipo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     tipo ENUM('Cabeza', 'Manos', 'Pies', 'Torso', 'ArmaPrimaria', 'ArmaSecundaria') NOT NULL,
     descripcion TEXT,
-    rareza ENUM('Common, Rare, Epic, Legendary') NOT NULL,
+    rareza ENUM('Common, Rare, Epic, Legendary') NOT NULL
 );
 
 CREATE TABLE Stats (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    equipo_id INT,
-    vida INT DEFAULT 0,
-    ataque INT DEFAULT 0,
-    defensa INT DEFAULT 0,
-    suerte INT DEFAULT 0,
-    velocidad INT DEFAULT 0,
-    resistencia INT DEFAULT 0,
-    efectividad INT DEFAULT 0,
+    equipo_id INT NOT NULL,
+    vida INT NOT NULL DEFAULT 0 CHECK (vida BETWEEN 0 AND 999),
+    ataque INT NOT NULL DEFAULT 0 CHECK (ataque BETWEEN 0 AND 999),
+    defensa INT NOT NULL DEFAULT 0 CHECK (defensa BETWEEN 0 AND 999),
+    suerte INT NOT NULL DEFAULT 0 CHECK (suerte BETWEEN 0 AND 999),
+    velocidad INT NOT NULL DEFAULT 0 CHECK (velocidad BETWEEN 0 AND 500),
+    resistencia INT NOT NULL DEFAULT 0 CHECK (resistencia BETWEEN 0 AND 999),
+    efectividad INT NOT NULL DEFAULT 0 CHECK (efectividad BETWEEN 0 AND 999),
     FOREIGN KEY (equipo_id) REFERENCES Equipo(id) ON DELETE CASCADE
 );
